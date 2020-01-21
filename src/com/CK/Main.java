86659f59c7cc4907a28edce7086a72afe525c861
CK <kainingc@usc.edu>
2020-01-21 11:31:29 -0800

    first commit

---
+++ src/com/CK/Main.java
@@ -0,0 +1,51 @@
+package com.CK;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class Main {
+
+    public static void main(String[] args) {
+        format f = new format();
+        f.m1();
+        f.m2();
+        f.m3();
+        f.m4();
+        f.m5();
+    }
+}
+
+class format {
+    double f = 111231.5585, nf = 10.00;
+    public void m1() {
+        BigDecimal bg = new BigDecimal(f);
+        double f1 = bg.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+        System.out.println(f1);
+    }
+    /**
+     * DecimalFormat转换最简便
+     */
+    public void m2() {
+        DecimalFormat df = new DecimalFormat("#.00");
+        System.out.println(df.format(f));
+    }
+    /**
+     * String.format打印最简便
+     */
+    public void m3() {
+        System.out.println(String.format("%.2f", f));
+    }
+    public void m4() {
+        NumberFormat nf = NumberFormat.getNumberInstance();
+        nf.setMaximumFractionDigits(2);
+        System.out.println(nf.format(f));
+    }
+
+    public void m5() {
+        double  discount = nf * 0.2;
+        if (discount % Math.floor(discount) == 0)
+            System.out.println((int)discount);
+    }
+
+}
